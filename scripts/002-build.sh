26d141b44dd674be8890d961cef42f47e375433c
Christian Berendt <berendt@osism.tech>
2023-02-17 21:08:23 +0100

    Do not cleanup cloud-init
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- scripts/002-build.sh
+++ scripts/002-build.sh
@@ -6,13 +6,10 @@ export DIB_CLOUD_INIT_DATASOURCES="ConfigDrive, OpenStack"
 
 export DIB_RELEASE=jammy
 
-export DIB_DEV_USER_USERNAME=ubuntu
-export DIB_DEV_USER_PWDLESS_SUDO=yes
-
 disk-image-create \
   -a amd64 \
   -o ci-image \
   --image-size 8 \
-  vm ubuntu ci-image devuser
+  vm ubuntu ci-image
 
 qemu-img info ci-image.qcow2
