7e8a0dbc583f720c3f3669d999f04dbba3828418
Christian Berendt <berendt@osism.tech>
2023-02-17 20:44:24 +0100

    Use jammy & add devuser (#12)
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- scripts/002-build.sh
+++ scripts/002-build.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 
 export ELEMENTS_PATH=./elements
+
 export DIB_CLOUD_INIT_DATASOURCES="ConfigDrive, OpenStack"
+
+export DIB_RELEASE=jammy
+
+export DIB_DEV_USER_USERNAME=ubuntu
+export DIB_DEV_USER_PWDLESS_SUDO=yes
 
 disk-image-create \
   -a amd64 \
   -o ci-image \
   --image-size 8 \
-  vm ubuntu ci-image
+  vm ubuntu ci-image devuser
 
 qemu-img info ci-image.qcow2
